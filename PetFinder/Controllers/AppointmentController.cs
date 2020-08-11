2ef35d12672420b9c654688a3307f3dfd14c9a2c
M-JM <jimmymiels@hotmail.com>
2020-08-11 21:18:28 +0200

    Appointment controller with comments and todo list. Added try catches and Error handling. Further completed sending email functionality.

--- PetFinder/Controllers/AppointmentController.cs
+++ PetFinder/Controllers/AppointmentController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -15,7 +17,7 @@ namespace PetFinder.Controllers
     public class AppointmentController : Controller
     {
         private readonly IPetRepository _petRepository;
-     
+
         private readonly ILogger<PetController> _logger;
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly IAppointmentRepository _appointmentRepository;
@@ -34,119 +36,173 @@ namespace PetFinder.Controllers
             _appointmentRepository = appointmentRepository;
             _emailService = emailService;
         }
+        //INFO
+
+        //https://stackify.com/csharp-exception-handling-best-practices/
+        // NullExceptionhandling is caught by the try catch. Brief look into info tells that try catch are expensive ressource wise (The entire stack has to be unwound).
+        // The info Null check vs. Try catch is pretty dated >5y, more recent info could not directly be found
+        // Topic to further investigate and understand what the best pratice is for NullException handling
+        // -> intresting is also "error monitoring service" i.e a tool (check out) 
+
+        //Explicit and Implicit Typing
+        
+        // There are situation where implicit Type is useful -> anonymous types and LINQ -> when type of variable is not known or does not exist (investigate further)
+        // Else always use Explicit type of variable -> better readability and maintainability.
+
+        //TODO 
+
+        //From high priority to low 
+
+        /// 1. Get the hours to block selectlistitems of TimePicker.
+        // Find out why Datepicker does not fire function to pass Date to GetHours Method ???
+        // The rest of method is straight foward -> get date from Datepicker , send it via AJAX post to method gethours.
+        // Retrieve list of hours provided by method gethours , push in Array in JS and set this Array to the property blocked hours of Timepicker.
+
+        /// 2. Fix placeholders for date/time in create booking
+        // Date and Startime are set in the createviewModel to have initial values in inputfields
+        // Since both values are non-nullable C# automatically assign them a default value ( default(DateTime) & default(StartTime))
+        // Making them nullable will allow to put placeholders like DD-MM-YY & HH-MM in the inputfields to have be able to perform validation on model
+        // Currently if the user saves accidently the form without selecting their own values , it will pass validation of the POST method.
+        //-> changing the Appointment model in DAL to nullable requires changes across all methods involved.
+        // by making it required i would still make sure no nulls value are saved in DB ( bad practice ?)
+        // maybe make a nullable in Viewmodel with other name and not direct property inherited from appointment would work.( bad pratice ??)
+
+        ///3. Fix Enddate on Create Post method
+        // instead of adding 1 hour through built in method -> change into adding one hour to end date in form before post.
+
+        ///4. Refactor GetEvents for Calendar
+        // Calendar lib takes a well defined class to show the events.
+        // Appointment class could be modified so the properties have the correct name so Calendar gets JSON feed with proper property name
+        // This would avoid to have to make new CalendarEvents with repetition of Data that is at 90% already available through class Appointment...
+        // Alternative is creating an Array to map the values from response of method , does not work. -> Mapped event object are not being pushed in Array before the Calender has rendered
+        // thus Calendar uses empty Array and return no appointments...
+
+        ///5.Is it ok to instead of Throw to return View Error ? Does it lose important infomation in Dev. env ? (Test this out).
+        // the exception is being logged though, so might not be to bad??
+
+        ///6. Make a proper email body -> fix date to only show date and time correctly. -> add shelter info maybe.
 
         public IActionResult List()
         {
-            List<AppointmentStatus> statuses = _appointmentRepository.GetStatus();
             // this gives the values for statusesdropdown in View 
-            // this is required so when the Edit Modal is called , the dropdown is populated with values from DB instead of hardcoding them in a select form-group.
+            // this is required so when the Edit Modal is called 
+            // the dropdown is populated with values from DB instead of hardcoding them in a select form-group.
+            List<AppointmentStatus> statuses = _appointmentRepository.GetStatus();
             StatusViewModel updateModel = new StatusViewModel(statuses)
             {
+            
             };
 
             return View(updateModel);
-
         }
         [HttpGet]
+        [Authorize(Roles = "User")]
         public IActionResult Create(int petid)
         {
-            Pet pet = _petRepository.GetById(petid);
-            AppointmentCreateViewModel model = new AppointmentCreateViewModel()
+            try
             {
-                PetId = pet.PetId,
-                AppointmentStatusId = 1,
-                // Any appointment created has the automatic status of Pending
-                ApplicationUserId = HttpContext.Session.GetString("id"),
-                ShelterId = pet.Shelter.ShelterId,
-                Date = DateTime.Now,
-                StartTime = new TimeSpan(8, 00, 00),
+                Pet pet = _petRepository.GetById(petid);
 
-                // Date and Startime are set in the createviewModel to have initial values in inputfields
-                // Since both values are non-nullable C# automatically assign them a default value ( default(DateTime) & default(StartTime))
-                // Making them nullable will allow to put placeholders like DD-MM-YY & HH-MM in the inputfields to have be able to perform validation on model
-                // Currently if the user saves accidently the form without selecting their own values , it will pass validation of the POST method.
-                //THIS IS NOT ACCEPTABLE -> changing the Appointment model in DAL to nullable requires changes across all methods involved.
-                // by making it required i would still make sure no nulls value are saved in DB ( bad practice ?)
-                // maybe make a nullable in Viewmodel with other name and not direct property inherited from appointment would work.( bad pratice ??)
-            };
-
-
-            return PartialView("Create",model);
+                AppointmentCreateViewModel model = new AppointmentCreateViewModel()
+                {
+                    // Any appointment created has the automatic status of Pending
+                    PetId = pet.PetId,
+                    AppointmentStatusId = 1,
+                    ApplicationUserId = HttpContext.Session.GetString("id"),
+                    ShelterId = pet.Shelter.ShelterId,
+                    Date = DateTime.Now,
+                    StartTime = new TimeSpan(8, 00, 00),
+                };
+                return PartialView("Create", model);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"When trying to create an appointment.");
+                return View("error");
+             
+            }
         }
 
         [HttpPost]
+        [Authorize(Roles = "User")]
         public async Task<IActionResult> CreateAsync(AppointmentCreateViewModel model)
         {
-            if (ModelState.IsValid)
+            try
             {
-                var pet = _petRepository.GetById(model.PetId);
-                var currentuser =  await _userManager.FindByIdAsync(HttpContext.Session.GetString("id"));
-                string emailbody = "Your Booking with " + pet.Name + " has been succesfully received";
-
-                // instead of adding 1 hour through built in method -> change into adding one hour to end date in form before post.
-
-                TimeSpan endTime = model.StartTime.Add(new TimeSpan(1, 0, 0));
-
-                Appointment appointment = new Appointment
+                if (ModelState.IsValid)
                 {
-                    StartTime = model.StartTime,
-                    EndTime = endTime,
-                    Date = model.Date,
-                    ApplicationUserId = model.ApplicationUserId,
-                    ShelterId = model.ShelterId,
-                    PetId = model.PetId,
-                    AppointmentStatusId = model.AppointmentStatusId
+                    Pet pet = _petRepository.GetById(model.PetId);
+                    ApplicationUser currentuser = await _userManager.FindByIdAsync(HttpContext.Session.GetString("id"));
+                    string emailbody = "Your Booking with " + pet.Name + " has been succesfully received";
 
+                    TimeSpan endTime = model.StartTime.Add(new TimeSpan(1, 0, 0));
 
+                    Appointment appointment = new Appointment
+                    {
+                        StartTime = model.StartTime,
+                        EndTime = endTime,
+                        Date = model.Date,
+                        ApplicationUserId = model.ApplicationUserId,
+                        ShelterId = model.ShelterId,
+                        PetId = model.PetId,
+                        AppointmentStatusId = model.AppointmentStatusId
+                    };
 
-                };
-                
-               _appointmentRepository.AddAppointment(appointment);
+                    _appointmentRepository.AddAppointment(appointment);
 
-                await _emailService.SendAsync(currentuser.UserName, "Appointment - PetFinder", emailbody, true);
+                    await _emailService.SendAsync(currentuser.UserName, "Appointment - PetFinder", emailbody, true);
 
+                    return RedirectToAction("Details", "Pet", new { id = appointment.PetId });
 
-                return RedirectToAction("Details","Pet", new { id = appointment.PetId });
-            
+                }
+                return View(model);
             }
-            return View(model);
+            catch (Exception ex )
+            {
+                _logger.LogError(ex, $"When trying to create an appointment / post method.");
+                return View("error");
+            }
         }
-
+        [Authorize(Roles = "Admin")]
         public async Task<JsonResult> GetAppointmentsAsync()
         {
-            ApplicationUser user = await _userManager.FindByNameAsync(HttpContext.User.Identity.Name);
-            List<Appointment> appointments = _appointmentRepository.GetAppointments(user.ShelterId);
-            List<CalendarEvents> Listevents = new List<CalendarEvents>();
-
-
-            foreach (var appointment in appointments)
+            try
             {
-                // Calendar lib takes a well defined class to show the events.
-                // Appointment class could be modified so the properties have the correct name so Calendar gets JSON feed with proper property name
-                // This would avoid to have to make new CalendarEvents with repetition of Data that is at 90% already available through class Appointment...
+                ApplicationUser user = await _userManager.FindByNameAsync(HttpContext.User.Identity.Name);
+                List<Appointment> appointments = _appointmentRepository.GetAppointments(user.ShelterId);
+                List<CalendarEvents> Listevents = new List<CalendarEvents>();
 
-                CalendarEvents events = new CalendarEvents
+
+                foreach (Appointment appointment in appointments)
                 {
-                    AppointmentId = appointment.AppointmentId,
-                    Title = appointment.Pet.Name,
-                    User = appointment.ApplicationUser.Email,
-                    BackgroundColor = appointment.AppointmentStatus.Color,
-                    Status = appointment.AppointmentStatus.StatusName,
-                    Start = appointment.Date + appointment.StartTime,
-                    End = appointment.Date + appointment.EndTime,
-                    allDay = false,
+
+                    CalendarEvents events = new CalendarEvents
+                    {
+                        AppointmentId = appointment.AppointmentId,
+                        Title = appointment.Pet.Name,
+                        User = appointment.ApplicationUser.Email,
+                        BackgroundColor = appointment.AppointmentStatus.Color,
+                        Status = appointment.AppointmentStatus.StatusName,
+                        Start = appointment.Date + appointment.StartTime,
+                        End = appointment.Date + appointment.EndTime,
+                        allDay = false,
+                    };
+                    Listevents.Add(events);
                 };
-                Listevents.Add(events);
-            };
 
-            //  var json = JsonConvert.SerializeObject(appointments);
+                //  var json = JsonConvert.SerializeObject(appointments);
+                // Avoid doing the above  and passing the json in the JSON method return, as JSON() already serialize the object given as parameter.
+                // having two serialization causes the JSON structure to be different Array of Array with Index and value(array of objects )thus requires unnecessary long pathing to get to values of properties 
 
-            // Avoid doing the above  and passing the json in the JSON method return, as JSON() already serialize the object given as parameter.
-            // having two serialization causes the JSON structure to be different Array of Array with Index and value(array of objects )thus requires unnecessary long pathing to get to values of properties 
-
-            return Json(Listevents);
+                return Json(Listevents);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"When trying to retrieve appointments / post method.");
+                throw;
+            }
         }
-
+        [Authorize(Roles = "Admin")]
         [HttpPost]
         public async Task<JsonResult> SaveEventAsync(CalendarEvents Updatedevent)
         {
@@ -157,44 +213,50 @@ namespace PetFinder.Controllers
             Appointment appointment = _appointmentRepository.GetAppointment(Updatedevent.AppointmentId);
             ApplicationUser usertoreceiveEmail = await _userManager.FindByIdAsync(appointment.ApplicationUserId);
 
-                appointment.AppointmentStatusId = Updatedevent.appointmentstatusId;
-            string emailbody ="";
+            appointment.AppointmentStatusId = Updatedevent.appointmentstatusId;
+            Appointment response = _appointmentRepository.UpdateAppointment(appointment);
+            Boolean status = false;
 
-
-            var response = _appointmentRepository.UpdateAppointment(appointment);
-            
-            if( response != null)
+            if (response != null && new int[] { 2, 3, }.Contains(response.AppointmentStatusId))
             {
-                switch (response.AppointmentStatusId) { 
+                string emailbody ="";
+
+                switch (response.AppointmentStatusId)
+                {
 
                     case 3:
 
                         emailbody = "Your Appointment with " + appointment.Pet.Name + " at " + appointment.Shelter.Name + " on " + response.Date + " has been confirmed";
 
-                    break;
+                        break;
 
                     case 2:
 
                         emailbody = "Your Appointment with " + appointment.Pet.Name + " at " + appointment.Shelter.Name + " on " + response.Date + " has been Rejected";
-
+                        
                         break;
-
+             
                 }
+
+                await _emailService.SendAsync(usertoreceiveEmail.UserName, "Appointment - PetFinder", emailbody, true);
 
             }
 
-            await _emailService.SendAsync(usertoreceiveEmail.UserName, "Appointment - PetFinder", emailbody, true);
+            if (response != null)
+            {
+                status = true;
+            }
 
-            Boolean status = true;
+           
 
             return Json(status);
         }
 
+        // Non Functional see-> TODO
         [HttpGet]
-
         public JsonResult GetBlockHours(int PetId, DateTime date)
         {
-           var appointments =  _appointmentRepository.GetHoursofAppointment(PetId, date);
+            List<Appointment> appointments = _appointmentRepository.GetHoursofAppointment(PetId, date);
 
             return Json(appointments);
 
